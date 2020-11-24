bcdf170ad7bba18fd8994fb6ed949ae2e7c60840
Mahmoud Badran <mahmoudbd@gmail.com>
2020-11-24 03:33:16 +0100

    added a productControlle

--- backend/routes/productsRouts.js
+++ backend/routes/productsRouts.js
@@ -1,30 +1,16 @@
 const express = require('express');
-// const dummyProducts = require('../data/dummyProducts');
-const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const Product = require('../models/productModel');
+const {
+	getProducts,
+	getProductById
+} = require('../controllers/productController');
 
 // Fetch all Products
-router.get(
-	'/',
-	asyncHandler(async (req, res) => {
-		const products = await Product.find({});
-		res.json(products);
-	})
-);
+// router.get('/', getProducts);
+router.route('/').get(getProducts);
 
 //Fetch a single product
-router.get(
-	'/:id',
-	asyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id);
-		if (product) {
-			res.json(product);
-		} else {
-			res.status(404);
-			throw new Error('Product Not Found...');
-		}
-	})
-);
+// router.get('/:id', getProductById);
+router.route('/:id').get(getProductById);
 
 module.exports = router;
