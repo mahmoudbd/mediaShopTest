584be71e1c910a7cf14686727fa27ff966c97410
Mahmoud Badran <mahmoudbd@gmail.com>
2020-11-30 17:33:05 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Media-Shop App 
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+Runs the app in the development mode ###`Frontend & Backend`.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run client`
+Runs the app in the development mode ###`Frontend`.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+
+
+### `npm run server`
+Runs the app in the development mode ###`Backend`.\
+Open [http://localhost:5000](http://localhost:5000) to view it in the browser.
+
+
+
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container, Form, FormControl } from 'react-bootstrap';
+import {
+	Navbar,
+	Nav,
+	Container,
+	Form,
+	FormControl,
+	NavDropdown
+} from 'react-bootstrap';
 import logo from '../logo.jpg';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../actions/userActions';
+
 function Header() {
+	const dispatch = useDispatch();
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
+	const logoutHandler = () => {
+		dispatch(logout());
+	};
 	return (
 		<header>
 			<React.Fragment>
@@ -43,11 +60,22 @@ function Header() {
 										<i className="fas fa-shopping-cart" />Cart
 									</Nav.Link>
 								</LinkContainer>
-								<LinkContainer to="/login">
-									<Nav.Link>
-										<i className="fas fa-user" />Sign In
-									</Nav.Link>
-								</LinkContainer>
+								{userInfo ? (
+									<NavDropdown title={userInfo.name} id="username">
+										<LinkContainer to="/profile">
+											<NavDropdown.Item>Profile</NavDropdown.Item>
+										</LinkContainer>
+										<NavDropdown.Item onClick={logoutHandler}>
+											Logout
+										</NavDropdown.Item>
+									</NavDropdown>
+								) : (
+									<LinkContainer to="/login">
+										<Nav.Link>
+											<i className="fas fa-user" />Sign In
+										</Nav.Link>
+									</LinkContainer>
+								)}
 							</Nav>
 						</Navbar.Collapse>
 					</Container>
