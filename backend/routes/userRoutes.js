683921eb8357d2abaf46ce1dc5d8c536b38c41a2
Mahmoud Badran <mahmoudbd@gmail.com>
2020-12-03 04:36:14 +0100

    update user profile backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const {
 	authUser,
 	getUserProfile,
-	registerUser
+	registerUser,
+	updateUserProfile
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 router.post('/login', authUser);
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
+
 module.exports = router;
