e4f889992b67f92034e559146fa58a73aec2a673
Mahmoud Badran <mahmoudbd@gmail.com>
2020-12-20 14:11:11 +0100

    create & update product admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,4 +30,56 @@ const deleteProduct = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { getProducts, getProductById, deleteProduct };
+//Create product  admin
+const createProduct = asyncHandler(async (req, res) => {
+	const product = await Product.create({
+		name: 'Sample name',
+		price: 0,
+		user: req.user._id,
+		image: '/image/sample.jpg',
+		brand: 'Sample brand',
+		category: 'Sample category',
+		countInStock: 0,
+		numRivews: 0,
+		description: 'Sample description'
+	});
+	const createdProduct = await product.save();
+	res.status(201).json(createdProduct);
+});
+
+//Update Product  admin
+const updateProduct = asyncHandler(async (req, res) => {
+	const {
+		name,
+		price,
+		description,
+		image,
+		brand,
+		category,
+		countInStock
+	} = req.body;
+
+	const product = await Product.findById(req.params.id);
+	if (product) {
+		(product.name = name),
+			(product.price = price),
+			(product.description = description),
+			(product.image = image),
+			(product.brand = brand),
+			(product.category = category),
+			(product.countInStock = countInStock);
+
+		const updatedProduct = await product.save();
+		res.status(201).json(updatedProduct);
+	} else {
+		res.json(404);
+		throw new Error('Product not found');
+	}
+});
+module.exports = {
+	getProducts,
+	getProductById,
+	deleteProduct,
+	createProduct,
+	updateProduct
+};
