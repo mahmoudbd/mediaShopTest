70a1d0f1509d2ec17f835b2ee5e9c4f0b90f117d
Mahmoud Badran <mahmoudbd@gmail.com>
2021-01-08 20:35:51 +0100

    fixed bug

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 dotenv.config();
 connectDB();
 
-// if (process.env.NODE_ENV === 'development') {
+// if (process.env.NODE_ENV === 'production') {
 // 	app.use(morgan('dev'));
 // }
 
@@ -36,9 +36,11 @@ app.get('/api/config/paypal', (req, res) =>
 app.use('/uploads', express.static(path.join('uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/frontend/build')));
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+		res.sendFile(
+			path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
+		);
 	});
 } else {
 	app.get('/', (req, res) => {
