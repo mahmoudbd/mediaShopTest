5dca5c701f6b4d7fb9571a6d65a839b845c2e9d2
Mahmoud Badran <mahmoudbd@gmail.com>
2021-01-08 20:20:53 +0100

    fixed

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,9 @@ app.get('/api/config/paypal', (req, res) =>
 app.use('/uploads', express.static(path.join('uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../frontend/build')));
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
 	app.get('*', (req, res) => {
-		res.sendFile(
-			path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
-		);
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 	});
 } else {
 	app.get('/', (req, res) => {
